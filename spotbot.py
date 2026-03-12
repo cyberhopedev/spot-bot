bf1751df15b87d164c1d25c9a57023dd9c74c86b
hkspence <hkspence@ncsu.edu>
2026-03-11 23:31:01 -0400

    [Implementation][Skeleton] Created skeleton for the discord bot

---
+++ spotbot.py
@@ -0,0 +1,46 @@
+"""
+    Author:  cyberhopedev
+    Date:    March 2026
+
+    Program:  SpotBot
+    Purpose:  A Discord bot that reads messages from a specific channel. If that message has
+    a Spotify link, it will add it to a specified Spotify playlist.
+
+#== ACKNOWLEDGEMENTS =========================================================
+Discord.py documentation: https://discordpy.readthedocs.io/en/stable/
+Spotipy documentation: https://spotipy.readthedocs.io/en/2.19.0/
+#=============================================================================
+
+"""
+import spotipy
+import discord
+
+class SpotBot(discord.Client):
+    """
+    A Discord bot that reads messages from a specific channel. If that message has
+    a Spotify link, it will add it to a specified Spotify playlist.
+    """
+    def __init__():
+        pass
+
+    def on_music_recs_message(self, message):
+        pass
+
+    def add_song_to_playlist(self, song_url):
+        pass
+
+
+    def get_song_id_from_url(self, song_url):
+        pass
+
+    def get_playlist_id_from_url(self, playlist_url):
+        pass
+
+    def get_user_id_from_url(self, user_url):
+        pass
+
+    def main():
+        pass
+
+    if __name__ == "__main__":
+        main()
